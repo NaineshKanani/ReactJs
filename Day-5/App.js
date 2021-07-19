4fa6518d3b687f86929817d8eb911f6d8da95a0f
NaineshKanani <87408081+NaineshKanani@users.noreply.github.com>
2021-07-19 11:41:28 +0530

    Add files via upload

--- Day-5/App.js
+++ Day-5/App.js
@@ -1,1 +1,22 @@
+import React from 'react'
+import NavBar from './Components/Navbar'
+import HeroSection from './Components/HeroSection'
+import FeatureSection from './Components/FeatureSection'
+import CardSection from './Components/CardSection'
+import FooterSection from './Components/FooterSection'
+import './App.css';
 
+function App() {
+  return (
+    <div>
+        <NavBar />
+        <FeatureSection />
+        <HeroSection />
+        <CardSection />
+        <FooterSection />
+
+    </div>
+  )
+}
+
+export default App;
